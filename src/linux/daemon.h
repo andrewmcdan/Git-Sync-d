8d034ce4d052abd8897dc1895c8db7162fba48d4
Andrew <andrewmcdan@gmail.com>
2023-12-28 10:47:20 -0500

    test CI again

--- src/linux/daemon.h
+++ src/linux/daemon.h
@@ -3,7 +3,10 @@
 
 #include <unistd.h>
 #include <sys/types.h>
+#include <functional>
+#include <string>
+#include "../common/error.h"
 
-void StartLinuxDaemon(int startCode, int argc, char** argv);
+void StartLinuxDaemon(int startCode, int argc, char** argv, std::function<void(std::string, GIT_SYNC_D_ERROR::_ErrorCode)> logEvent);
 
 #endif // LINUX_DAEMON_H
--- src/mac/daemon.h
+++ src/mac/daemon.h
@@ -1,7 +1,10 @@
 #ifndef MAC_DAEMON_H
 #define MAC_DAEMON_H
 
+#include <functional>
+#include <string>
+#include "../common/error.h"
 
-void StartMacDaemon(int startCode, int argc, char** argv);
+void StartMacDaemon(int startCode, int argc, char** argv, std::function<void(std::string, GIT_SYNC_D_ERROR::_ErrorCode)> logEvent);
 
 #endif // LINUX_DAEMON_H
